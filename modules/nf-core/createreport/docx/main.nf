6665bbcd5441197a04e131ed9181effedae9d8d2
iyamaguchi <yamaguchidate21@yahoo.co.jp>
2024-02-11 13:57:37 +0900

    modified createreport module

--- modules/nf-core/createreport/docx/main.nf
+++ modules/nf-core/createreport/docx/main.nf
@@ -35,20 +35,23 @@ process CREATEREPORT_DOCX {
     //               https://github.com/nf-core/modules/blob/master/modules/nf-core/bwa/index/main.nf
     // TODO nf-core: Where applicable please provide/convert compressed files as input/output
     //               e.g. "*.fastq.gz" and NOT "*.fastq", "*.bam" and NOT "*.sam" etc.
-    tuple val(meta), path(bam)
+    path(customer_info)
+    path(software_versions)
+    path(docx_template)
+    path(workflow_picture)
 
     output:
     // TODO nf-core: Named file extensions MUST be emitted for ALL output channels
-    tuple val(meta), path("*.bam"), emit: bam
+    path("*.docx")                , emit: report
+    path("all_informations.yml")  , emit: informations
     // TODO nf-core: List additional required output channels/values here
     path "versions.yml"           , emit: versions
 
     when:
     task.ext.when == null || task.ext.when
 
-    script:
+    script: // This script is bundled with the pipeline, in nf-core/mypipeline/bin/
     def args = task.ext.args ?: ''
-    def prefix = task.ext.prefix ?: "${meta.id}"
     // TODO nf-core: Where possible, a command MUST be provided to obtain the version number of the software e.g. 1.10
     //               If the software is unable to output a version number on the command-line then it can be manually specified
     //               e.g. https://github.com/nf-core/modules/blob/master/modules/nf-core/homer/annotatepeaks/main.nf
@@ -58,34 +61,23 @@ process CREATEREPORT_DOCX {
     //               using the Nextflow "task" variable e.g. "--threads $task.cpus"
     // TODO nf-core: Please replace the example samtools command below with your module's command
     // TODO nf-core: Please indent the command appropriately (4 spaces!!) to help with readability ;)
-    """
-    samtools \\
-        sort \\
-        $args \\
-        -@ $task.cpus \\
-        -o ${prefix}.bam \\
-        -T $prefix \\
-        $bam
+    //
 
-    cat <<-END_VERSIONS > versions.yml
-    "${task.process}":
-        createreport: \$(samtools --version |& sed '1!d ; s/samtools //')
-    END_VERSIONS
-    """
+    template 'make_docx_report.py'
+
 
     stub:
     def args = task.ext.args ?: ''
-    def prefix = task.ext.prefix ?: "${meta.id}"
     // TODO nf-core: A stub section should mimic the execution of the original module as best as possible
     //               Have a look at the following examples:
     //               Simple example: https://github.com/nf-core/modules/blob/818474a292b4860ae8ff88e149fbcda68814114d/modules/nf-core/bcftools/annotate/main.nf#L47-L63
     //               Complex example: https://github.com/nf-core/modules/blob/818474a292b4860ae8ff88e149fbcda68814114d/modules/nf-core/bedtools/split/main.nf#L38-L54
     """
-    touch ${prefix}.bam
+    touch report.docx all_informations.yml
 
     cat <<-END_VERSIONS > versions.yml
     "${task.process}":
-        createreport: \$(samtools --version |& sed '1!d ; s/samtools //')
+        python: \$(python --version | sed 's/Python //g')
     END_VERSIONS
     """
 }
---
+++ modules/nf-core/createreport/docx/templates/make_docx_report.py
@@ -0,0 +1,312 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys, os, shutil, re, csv, yaml, json, argparse
+import pandas as pd
+from pathlib import Path
+from datetime import datetime
+from docx import Document
+from docx.shared import Inches, Cm
+from docx.enum.table import WD_ROW_HEIGHT, WD_ROW_HEIGHT_RULE
+
+dtnow = datetime.now()
+
+class DisplayablePath(object):
+    display_filename_prefix_middle = '├──'
+    display_filename_prefix_last = '└──'
+    display_parent_prefix_middle = '    '
+    display_parent_prefix_last = '│   '
+
+    def __init__(self, path, parent_path, is_last):
+        self.path = Path(str(path))
+        self.parent = parent_path
+        self.is_last = is_last
+        if self.parent:
+            self.depth = self.parent.depth + 1
+        else:
+            self.depth = 0
+
+    @property
+    def displayname(self):
+        if self.path.is_dir():
+            return self.path.name + '/'
+        return self.path.name
+
+    @classmethod
+    def make_tree(cls, root, parent=None, is_last=False, criteria=None):
+        root = Path(str(root))
+        criteria = criteria or cls._default_criteria
+
+        displayable_root = cls(root, parent, is_last)
+        yield displayable_root
+
+        children = sorted(list(path
+                            for path in root.iterdir()
+                            if criteria(path)),
+                            key=lambda s: str(s).lower())
+        count = 1
+        for path in children:
+            is_last = count == len(children)
+            if path.is_dir():
+                yield from cls.make_tree(path,
+                                        parent=displayable_root,
+                                        is_last=is_last,
+                                        criteria=criteria)
+            else:
+                yield cls(path, displayable_root, is_last)
+            count += 1
+
+    @classmethod
+    def _default_criteria(cls, path):
+        return( re.match('\..*', path.name) is None )
+
+    @property
+    def displayname(self):
+        if self.path.is_dir():
+            return self.path.name + '/'
+        return self.path.name
+
+    def displayable(self):
+        if self.parent is None:
+            return self.displayname
+
+        _filename_prefix = (self.display_filename_prefix_last
+                            if self.is_last
+                            else self.display_filename_prefix_middle)
+
+        parts = ['{!s} {!s}'.format(_filename_prefix,
+                                    self.displayname)]
+
+        parent = self.parent
+        while parent and parent.parent is not None:
+            parts.append(self.display_parent_prefix_middle
+                        if parent.is_last
+                        else self.display_parent_prefix_last)
+            parent = parent.parent
+
+        return ''.join(reversed(parts))
+
+
+def getTree(mInfo):
+    paths = DisplayablePath.make_tree(Path(mInfo["tree_root"]))
+    return('\n'.join([path.displayable() for path in paths]))
+
+def testKeys_old(keys, text):
+    hitKeys = []
+    for key in keys:
+        braketkey = '{' + key + '}'
+        if (braketkey in text):
+            hitKeys.append(key)
+    return(hitKeys)
+
+def testKeys(keys, text):
+    def tester(key):
+        return( ('{' + key + '}') in text )
+
+    return(list(filter(tester, keys)))
+
+def modDocument(doc, mInfo):
+
+    tags = {
+            'DATE' : dtnow.strftime("%Y-%m-%d"),
+            'CUSTOMERNAME' : mInfo["customer_name"],
+            'CUSTOMERINSTITUTE' : mInfo["customer_inst"].split(" ")[0],
+            'ANALYSIS' : mInfo["analysis"],
+            'SEQUENCER' : mInfo["sequencer"],
+            'RRNA' : mInfo["rRNA_removal_kit"],
+            'LIBRARY' : mInfo["library_preparation_kit"],
+#          'DIRTREE' : getTree(mInfo),
+            'COMMONNAME' : mInfo["organism"],
+#          'SCIENTIFICNAME' : mInfo["scientific_name"],
+            'GENOMEASSEMBLY' : mInfo["reference"],
+        }
+    keys = tags.keys()
+
+    for para in doc.paragraphs:
+        hitKeys = testKeys(tags, para.text)
+        if len(hitKeys) > 0:
+#            print("{}: {}".format(hitKeys, para.text))
+            inline = para.runs
+            # Loop added to work with runs (strings with same style)
+            for i in range(len(inline)):
+                textMod = inline[i].text.replace('{','').replace('}','')
+                for hitKey in hitKeys:
+                    textMod = textMod.replace(hitKey, tags[hitKey])
+#                print("({}) {} ===> {}".format(i, inline[i].text, textMod))
+                inline[i].text = textMod
+
+def modPictutre(doc):
+
+    tags = {
+            'WORKFLOW' : ["workflow.png", 15, 20, "図1. 解析ワークフロー"],
+            'HEATMAP' : ["heatmap.png", 15, 10, "図2. 発現遺伝子のクラスタリング結果(ヒートマップ)"]
+        }
+    keys = tags.keys()
+
+    for para in doc.paragraphs:
+        hitKeys = testKeys(tags, para.text)
+        if len(hitKeys) > 0:
+#            print("{}: {}".format(hitKeys, para.text))
+            inline = para.runs
+            # Loop added to work with runs (strings with same style)
+            for hitKey in hitKeys:
+                textMod1 = inline[0].text.replace('{','').replace('}','').replace(hitKey, '')
+                textMod2 = inline[1].text.replace('{','').replace('}','').replace(hitKey, tags[hitKey][3])
+                inline[0].text = textMod1
+                inline[1].text = textMod2
+                inline[0].add_picture(tags[hitKey][0], width=Cm(tags[hitKey][1]), height=Cm(tags[hitKey][2]))
+
+            #for i in range(len(inline)):
+            #    textMod = inline[i].text.replace('{','').replace('}','')
+            #    for hitKey in hitKeys:
+            #        textMod = textMod.replace(hitKey, tags[hitKey][0])
+            #    print("({}) {} ===> {}".format(i, inline[i].text, textMod))
+            #    inline[i].text = textMod
+
+def readMappingStat(mappingStat):
+    with open(mappingStat, 'r') as f:
+        reader = csv.reader(f, delimiter = '\t')
+        header = next(reader)
+        data = []
+        for row in reader:
+            rowT = row[0].split('_')
+            rowD = list(map(lambda d: "{:,}".format(int(d)), row[2:5:]))
+            data.append(rowT + rowD)
+        return(data)
+
+def addMappingTable(table, mappingStat):
+    mtx = readMappingStat(mappingStat)
+    # mtx_sorted = sorted(mtx, key = lambda d: d[1])
+    # for drow in mtx_sorted:
+    for drow in mtx:
+        trow = table.add_row()
+        for cell, data in zip(trow.cells, drow):
+            cell.text = data
+        trow.height_rule = WD_ROW_HEIGHT_RULE.AUTO
+
+def readFragmentSize(fragmentSize):
+    with open(fragmentSize, 'r') as f:
+        reader = csv.reader(f, delimiter = '\t')
+        header = next(reader)
+        data = []
+        for row in reader:
+            data.append(row)
+        return(data)
+
+def addFragmentSize(table, fragmentSize):
+    mtx = readFragmentSize(fragmentSize)
+    # mtx_sorted = sorted(mtx, key = lambda d: d[1])
+    # for drow in mtx_sorted:
+    for drow in mtx:
+        trow = table.add_row()
+        for cell, data in zip(trow.cells, drow):
+            cell.text = data
+
+def readYaml(yml):
+    """
+    description: yamlを読み込んで、辞書を返す。
+    """
+    with open(yml, 'r') as f:
+        info = yaml.load(f, Loader = yaml.SafeLoader)
+    return info
+
+def makeAllInfo(cinfo, sinfo):
+    """
+    description: 全てのサンプル情報を結合した'all_informations.yml'を作成する
+    """
+    all_info = readYaml(cinfo)
+    all_info['SOFTWARE_VERSIONS'] = readYaml(sinfo)
+    return all_info
+
+def addInfoTable(table, json):
+    """
+    description: Library作製キット及びrRNA除去キットの情報を挿入する。
+
+    """
+    df = pd.DataFrame()
+    df['Step'] = ['rRNA removal', 'Library preparation']
+    df['Kit'] = [json['rRNA_removal_kit'], json['library_preparation_kit']]
+#    df =  df.set_index('Step')
+    for i in range(df.shape[0]):
+        trow = table.add_row()
+        for cell, data in zip(trow.cells, df.loc[i]):
+            cell.text = str(data)
+
+    table.height_rule = WD_ROW_HEIGHT_RULE.AUTO
+
+def main():
+    """
+    description: main関数
+    """
+
+    """インプットファイルの解析"""
+    customer_info = ""
+    software_versions = ""
+    template = ""
+    workflowpic = ""
+    outdir = '.'
+
+    """all_informations.ymlの作成"""
+    all_info = makeAllInfo(customer_info, software_versions)
+    with open("all_informations.yml", "w") as f:
+        yaml.dump(all_info, f, default_flow_style=False)
+
+    customer = all_info['CUSTOMER_INFORMATION']
+    experiments = all_info['EXPERIMENTS_INFORMATION']
+    organism = all_info['ORGANISM_INFORMATION']
+    samples = all_info['SAMPLE_INFORMATION']
+    tools = all_info['SOFTWARE_VERSIONS']
+
+    """docxファイルの作成"""
+    outfile = "{}/{}・{}_{}_report_{}.docx".format(
+        outdir,
+        customer["customer_inst"].split(" ")[0].replace(" ", "_"),
+        customer["customer_name"].replace(" ", "_"),
+        experiments['analysis'],
+        dtnow.strftime("%Y%m%d")
+    )
+
+    doc = Document(template)
+
+    style = doc.styles['Normal']
+    font = style.font
+    font.name = 'Arial'
+
+    workflow = doc.add_picture(workflowpic), width=Cm(15), height=Cm(20))
+    #heatmap =  doc.add_picture('heatmap.png', width=Cm(15), height=Cm(10))
+
+    #fastq = readJsonTable(fastqd, select=['actual_read_num', 'min_len' ,'avg_len', 'max_len'], csep=True)
+    #mapping = readJsonTable(samples,  select=['mapping_input_reads', 'unmapped_reads' ,'rrna_reads', 'total_mapped_reads', 'unique_mapped_reads'])
+
+    #modDocument(doc, analysis)
+    #modPictutre(doc)
+
+    #if 'STAR_RSEM' in os.path.basename(args.template):
+    #    addJsonTable(doc.tables[0], fastq)
+    #    #addJsonTable(doc.tables[1], mapping)
+    #    addMappingTable(doc.tables[1], star_mapping)
+    #    addMappingTable(doc.tables[2], rsem_mapping)
+    #    addInfoTable(doc.tables[3], analysis)
+    #else:
+    #    addJsonTable(doc.tables[0], fastq)
+        #addJsonTable(doc.tables[1], mapping)
+        #addMappingTable(doc.tables[1], star_mapping)
+    #    addInfoTable(doc.tables[1], analysis)
+
+
+    doc.save(outfile)
+
+    # write python and libraries version to yaml.
+    versions_this_module = {}
+    versions_this_module["${task.process}"] = {
+        "python": platform.python_version(),
+        "pandas": pd.__version__,
+        "yaml": yaml.__version__,
+        "docx": docx.__version__
+    }
+
+    with open("versions.yml", "w") as f:
+        yaml.dump(versions_this_module, f, default_flow_style=False)
+
+if __name__ == '__main__':
+    main()
